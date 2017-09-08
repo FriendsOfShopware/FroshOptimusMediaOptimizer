055316c7f8435f9dcdf3370f76515033fda9b05e
skoenig <s.koenig@tinect.de>
2017-09-09 01:30:25 +0200

    optimizing webp-handling for 5.2 and 5.3

--- Components/OptimusOptimizer.php
+++ Components/OptimusOptimizer.php
@@ -44,25 +44,49 @@ class OptimusOptimizer implements OptimizerInterface
      */
     public function run($filepath)
     {
-        $extension = strtolower($this->getImageExtension($filepath));
 
-        switch ($extension) {
-            case 'webp':
-                $jpgPath = $this->rootDir . str_replace('.webp', '.jpg', $filepath);
-                $pngPath = $this->rootDir . str_replace('.webp', '.png', $filepath);
+        //ass of SW5.3 media optimizer uses tmp-Folder
+        if (!$this->isShopware53()) {
+            $filepath = $this->rootDir . $filepath;
+        }
 
-                if (@file_exists($jpgPath)) {
-                    $this->optimusService->optimize($jpgPath,
+        $mime = mime_content_type($filepath);
+
+        switch ($mime) {
+            case 'image/webp':
+
+                if ($this->isShopware53()) {
+
+                    $im = imagecreatefromwebp($filepath);
+                    imagejpeg($im, $filepath . '.jpg', 100);
+                    imagedestroy($im);
+
+                    $this->optimusService->optimize($filepath . '.jpg',
                         'webp',
-                        $this->rootDir . $filepath);
-                } elseif (@file_exists($pngPath)) {
-                    $this->optimusService->optimize($pngPath,
-                        'webp',
-                        $this->rootDir . $filepath);
+                        $filepath);
+                    @unlink($filepath . '.jpg');
+
+                } else {
+
+                    //TODO: Test in 5.2.17
+                    $jpgPath = str_replace('.webp', '.jpg', $filepath);
+                    $pngPath = str_replace('.webp', '.png', $filepath);
+
+                    if (@file_exists($jpgPath)) {
+                        $this->optimusService->optimize($jpgPath,
+                            'webp',
+                            $filepath);
+                    } elseif (@file_exists($pngPath)) {
+                        $this->optimusService->optimize($pngPath,
+                            'webp',
+                            $filepath);
+                    }
+                    
                 }
+
                 break;
             default:
-                $this->optimusService->optimize($this->rootDir . $filepath);
+                $this->optimusService->optimize($filepath);
                 break;
         }
 
@@ -96,6 +120,29 @@ class OptimusOptimizer implements OptimizerInterface
     {
         $pathInfo = pathinfo($path);
         return $pathInfo['extension'];
+    }
+
+    /**
+     * Compare versions.
+     * @param string $version Like: 5.0.0
+     * @param string $operator Like: <=
+     *
+     * @return mixed
+     */
+    public function versionCompare($version, $operator)
+    {
+        // return by default version compare
+        return version_compare(Shopware()->Config()->get('Version'), $version, $operator);
+    }
+
+    /**
+     * Check if current environment is shopware 5.
+     *
+     * @return bool
+     */
+    public function isShopware53()
+    {
+        return $this->versionCompare('5.3.0', '>=');
     }
 
 
